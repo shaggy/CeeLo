3fce4ac41c1f335e71d19913c66f199dfccbf95c
shaggy <jdabrowskiromanczuk@gmail.com>
2020-06-09 18:32:14 -0400

    fixed comments, switched from array to arraylists, created instance of CeeLo class to call new non static main methods, changed loop to allow user to quit mid game

--- CeeLo.java
+++ CeeLo.java
@@ -1,28 +1,25 @@
-// shaggy (Jan Dabrowski)
-// first version of CeeLo Dice game
-import java.util.Random;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException; 
+// shaggy CeeLo game
+import java.util.*;
+import java.io.*;
+
 
 public class CeeLo
 {
    public static void main(String[] args) throws IOException
    {
       //declaration block
-      BufferedReader BR = new BufferedReader (new InputStreamReader(System.in));
+      BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
       
       Player human = new Player();
       Player computer = new Player();
+      CeeLo gameInstance = new CeeLo();
 
       double wallet = 100.0;
       double bet = 0.0;
-           
-   
+      String quit = "";
+      
       System.out.println("What's your name?:  ");
- 
-      human.setName(BR.readLine());
-       
+      human.setName(br.readLine());
       computer.setName("Computer");
       
       System.out.println("\nWelcome to Cee Lo " + human.getName() + "\n");
@@ -31,47 +28,53 @@ public class CeeLo
        
      
       do
+      
       {
       
-         System.out.println("\nWallet: " + wallet);
-            
-         System.out.println("How much would you like to bet?\n");
-      
-         bet = input(wallet);
+         System.out.println("\nWallet: " + wallet + "\t\t<<[Any key to bet, q to quit]" );
+         //System.out.println("([Any key to bet, q to quit])\n");
+                  
+         quit = br.readLine();
+        
+         if (quit.equals("q") || quit.equals("Q"))
+         {
+            break;
+         }
+         
+         System.out.println("Bet amount: ");
+         bet = gameInstance.input(wallet);
+         
            
          human.playerRolls();
-      
          computer.playerRolls();
-        
-         showRoll(human);
-         showRoll(computer);
+         gameInstance.showRoll(human);
+         gameInstance.showRoll(computer);
       
-         int scoreHuman = gameLogic(human.getDieValues(), human);
-         int scoreComputer = gameLogic(computer.getDieValues(), computer);
+         int scoreHuman = gameInstance.gameLogic(human.getDieValues(), human);
+         int scoreComputer = gameInstance.gameLogic(computer.getDieValues(), computer);
       
-         String victor = calculateWinner(scoreHuman,scoreComputer, human, computer);
+         String victor = gameInstance.calculateWinner(scoreHuman,scoreComputer, human, computer);
+         System.out.println("The winner is: " + victor);
          
-         System.out.println("\nThe winner is: " + victor);
-         
-         wallet = adjustWallet(victor, wallet, bet, human, computer);
+         wallet = gameInstance.adjustWallet(victor, wallet, bet, human, computer);
           
                                                                                   
      }  while (wallet > 0.0);
     
     System.out.println("\nWallet: " + wallet);
-    System.out.println("Goodbye!");
+    System.out.println("\nGoodbye!");
       
      
 
  }
- /**
-   @param int[] array = sorted array of all die values from player class 
-   @param Player x = player object
-   
-   gameLogic returns an int with the score a player has based on their roll
- 
- */
- public static int gameLogic(int[] array, Player x)
+/**
+*   gameLogic returns an int with the score a player has based on their roll
+*
+*   @param ArrayList<Integer> array = sorted array of all die values from player class 
+*   @param Player x = player object
+* 
+*/
+ public int gameLogic(ArrayList<Integer> array, Player x)
  {
    
    
@@ -79,35 +82,35 @@ public class CeeLo
    
    // 4, 5, 6 is an automatic win
    
-   if(array[0] == 4 && array[1] == 5 && array[2] == 6)
+   if(array.get(0) == 4 && array.get(1) == 5 && array.get(2) == 6)
    {
-      System.out.println(x.getName() + " won!");
+      System.out.println("456: Automatic win!\n" );
       playerScore = 100; //high number not achieved by any other rolls
    
    }
    // 1, 2, 3 is an automatic loss
-   else if (array[0] == 1 && array[1] == 2 && array[2] == 3)
+   else if (array.get(0) == 1 && array.get(1) == 2 && array.get(2) == 3)
     {
-         System.out.println(x.getName() + " loses!");
+         System.out.println("123: Automatic loss!\n");
          playerScore = -100; //no roll can give neg value
     }
    //sorted array may either have two of same number at beginning or end - leftover is score
-   else if (array[0] == array[1] && array[1] != array[2])
+   else if (array.get(0) == array.get(1) && array.get(1) != array.get(2))
    {
-      playerScore = array[2];
-      System.out.println(x.getName() + " got: " +  playerScore);
+      playerScore = array.get(2);
+      System.out.println(x.getName() + " got: " +  playerScore + "\n");
    }
-   else if (array[1] == array[2] && array[2] != array[0])
+   else if (array.get(1) == array.get(2) && array.get(2) != array.get(0))
    {
-      playerScore = array[0];
-      System.out.println(x.getName() + " got: " + playerScore);
+      playerScore = array.get(0);
+      System.out.println(x.getName() + " got: " + playerScore + "\n");
    
    }
    //triple of same number worth more than above scores. Multiply score by 10 to signify this. 
-   else if (array[0] == array[1] && array[1] == array[2])
+   else if (array.get(0) == array.get(1) && array.get(1) == array.get(2))
    {
-      System.out.println(x.getName() + " rolled trip " + array[0] + "'s!");
-      playerScore = array[0] * 10;
+      System.out.println(x.getName() + " rolled trip " + array.get(0) + "'s!\n");
+      playerScore = array.get(0) * 10;
    
    }
    // No score: roll, print roll, recursive call to gameLogic with new die values
@@ -118,20 +121,21 @@ public class CeeLo
       showRoll(x);
       playerScore = gameLogic(x.getDieValues(), x);
    }
+ x.getDieValues().clear();//empty arraylist so it doesn't get filled dynamically on next roll and same roll returned
  
  return playerScore;
  }
- /**
-   @param  int a = human's score
-   @param int b = computer's score
-   @param Player x = human player
-   @param Player ai = computer
-   
-   calculateWinner compares the scores attained by human and player and
-returns string with winner's name.
- 
- */
- public static String calculateWinner(int a, int b, Player x, Player ai)
+/**
+*   calculateWinner compares the scores attained by human and player and
+* returns string with winner's name.
+*
+*   @param int a = human's score
+*   @param int b = computer's score
+*   @param Player x = human player
+*   @param Player ai = computer
+*   
+*/
+ public String calculateWinner(int a, int b, Player x, Player ai)
  {
    String winner = " ";
    if(a > b)
@@ -155,15 +159,17 @@ returns string with winner's name.
    
  }
 /**
-@param String s = victor from main, which stores name of winner of round
-@param double max = wallet amount (initialized to 100 to start)
-@param double wager = bet amount
-@param Player x = human player
-@param Player ai = computer
-adjustWallet removes lost bet or adds winnings to wallet amount
+*   adjustWallet deducts lost bet or adds winnings to wallet amount
+*
+* @param String s = victor from main, which stores name of winner of round
+* @param double max = wallet amount (initialized to 100 to start)
+* @param double wager = bet amount
+* @param Player x = human player
+* @param Player ai = computer
+*
 */
  
- public static double adjustWallet(String s, double max, double wager, Player x, Player ai)
+ public double adjustWallet(String s, double max, double wager, Player x, Player ai)
  {
    if (s.equals(x.getName()))
          {
@@ -178,31 +184,38 @@ adjustWallet removes lost bet or adds winnings to wallet amount
 
 
 /**
-   @param Player x = player object instance
-   
-   showRoll prints out player's roll values.
+*   showRoll prints out player's roll values.
+*   
+*   @param Player x = player object instance
+*   
 */
- public static void showRoll(Player x)
- {
-      System.out.println(x.getName() + " rolls... ");
+ public void showRoll(Player x)
+ {    
+      final int SIZE = 3;
+      System.out.println(x.getName() + " rolls... \n");
          
-      for (int i = 0; i < 3; i++)
+      for (int i = 0; i < SIZE; i++)
       {
-         System.out.println(x.getDieValues()[i] + " ");
+         System.out.print(x.getDieValues().get(i) + " ");
       } 
       
-      System.out.println();
+      System.out.println("\n");
+      
  }
 /**
-@param double max = wallet amount
-input takes in the amount a player wants to bet and returns it if it's more than 0 and within wallet amount
+*     The input method takes in the amount a player wants to bet as a
+*  <code>String</code> and returns it as a <code>double</code> if it's more than
+*  zero and within the wallet amount.
+*
+* @param double max = wallet amount
+* @return value
 */
-public static double input(double max) throws IOException
+public double input(double max) throws IOException
    {
-      BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       double value;
       
-      value = Double.parseDouble(BR.readLine());;
+      value = Double.parseDouble(br.readLine());;
       
       if(value < 0 || value > max)
          value = validation(value, max);
@@ -210,20 +223,22 @@ public static double input(double max) throws IOException
       return value;
    }
 /**
-@param double x = amount a player wants to bet
-@param double max = wallet amount
-validation tells player if they input an amount 0 or less, or more than they have in the wallet 
-and takes in input until it meets the rules. Returns to input method. 
+*  validation tells player if they input an amount 0 or less, or more than they have in the wallet 
+* and takes in input until it meets the rules. Returns to input method. 
+*
+*
+* @param double x = amount a player wants to bet
+* @param double max = wallet amount
 */   
-public static double validation(double x, double max) throws IOException
+public double validation(double x, double max) throws IOException
    {
-      BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
       
       while( x < 0 || x > max)
       {
          System.out.println("\nValue must be greater than 0! Max bet = whole wallet.\n");
-         x = Double.parseDouble(BR.readLine());
+         x = Double.parseDouble(br.readLine());
          
       }
    
@@ -269,71 +284,52 @@ class Die
 class Player
 {
    private String name;
-   private int[] dieValues = new int[3];
+   private ArrayList<Integer> dieValues = new ArrayList<>();   
+   //final int size = 3;
+   //this.dieValues = new int[size];
+   private final int SIZE = 3;
+   private final int numSides = 6;
+
          
    void playerRolls()
    {
-      final int numDie = 3;
-      final int numSides = 6;
+      ArrayList<Die> dieArray = new ArrayList<>(SIZE);
+             
       
-      Die[] dieArray = new Die[numDie];
-      
-      for (int i = 0; i < dieArray.length; i++)
+      for (int i = 0; i < SIZE; i++)
       {
-         dieArray[i] = new Die(numSides);
+         dieArray.add(new Die(numSides));
+         
       }
       
-      for (int i = 0; i < dieArray.length; i++)
+      for (int i = 0; i < SIZE; i++)
       {
-         dieValues[i] = dieArray[i].getValue();
-      
+         
+         dieValues.add(i, dieArray.get(i).getValue());
+         
+
       }
       
-      selectionSort(dieValues);
+      Collections.sort(dieValues); 
      
      } 
     
     
-    int[] getDieValues()
+    ArrayList<Integer> getDieValues()
     {
       return dieValues;
     }
     
-    void setName(String pname)
+    void setName(String name)
     {
-      name = pname;
+      this.name = name;
     }
     
     String getName()
     {
       return name;
     }
-/**
-@param int[] array = array to be sorted
-selectionSort sorts an array's contents from lowest to highest
-*/    
-   void selectionSort(int[] array)
-   {
-      int startScan, index, minIndex, minValue;
-      
-      for (startScan = 0; startScan < array.length-1; startScan++)
-      {
-         minIndex = startScan;
-         minValue = array[startScan];
-         
-         for( index = startScan + 1; index < array.length; index++)
-         {
-            if(array[index] < minValue)
-            {
-               minValue = array[index];
-               minIndex = index;
-            }
-            
-            array[minIndex] = array[startScan];
-            array[startScan] = minValue;
-         
-         }
-      }
-   
-   }
 }
+
+
+
