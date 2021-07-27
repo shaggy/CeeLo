568a83f388dcf9f8f7a44c731d36c3bcda9c0561
Jan Dabrowski <jdabrowskiromanczuk@gmail.com>
2021-07-27 19:08:59 +0300

    Update CeeLo.java
    Modified for use in Java Swing app

--- CeeLo.java
+++ CeeLo.java
@@ -1,79 +1,16 @@
 // shaggy CeeLo game
 import java.util.*;
-import java.io.*;
-
 
 public class CeeLo
 {
-   public static void main(String[] args) throws IOException
-   {
-      //declaration block
-      BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-      
-      Player human = new Player();
-      Player computer = new Player();
-      CeeLo gameInstance = new CeeLo();
-
-      double wallet = 100.0;
-      double bet = 0.0;
-      String quit = "";
-      
-      System.out.println("What's your name?:  ");
-      human.setName(br.readLine());
-      computer.setName("Computer");
-      
-      System.out.println("\nWelcome to Cee Lo " + human.getName() + "\n");
-     
-     //------------------game loop------------------//
-       
-     
-      do
-      
-      {
-      
-         System.out.println("\nWallet: " + wallet + "\t\t<<[Any key to bet, q to quit]" );
-         //System.out.println("([Any key to bet, q to quit])\n");
-                  
-         quit = br.readLine();
-        
-         if (quit.equals("q") || quit.equals("Q"))
-         {
-            break;
-         }
-         
-         System.out.println("Bet amount: ");
-         bet = gameInstance.input(wallet);
-         
-           
-         human.playerRolls();
-         computer.playerRolls();
-         
-         gameInstance.showRoll(human);
-         gameInstance.showRoll(computer);
-      
-         int scoreHuman = gameInstance.gameLogic(human.getDieValues(), human);
-         int scoreComputer = gameInstance.gameLogic(computer.getDieValues(), computer);
-      
-         String victor = gameInstance.calculateWinner(scoreHuman,scoreComputer, human, computer);
-         System.out.println("The winner is: " + victor);
-         
-         wallet = gameInstance.adjustWallet(victor, wallet, bet, human, computer);
-         
-                                                                                  
-     }  while (wallet > 0.0);
-    
-    System.out.println("\nWallet: " + wallet);
-    System.out.println("\nGoodbye!");
-      
-     
-
- }
+ 
+ 
 /**
-*   gameLogic returns an int with the score a player has based on their roll
+*   gameLogic calculates score a player has based on their roll and CeeLo rules
 *
 *   @param ArrayList<Integer> array = sorted array of all die values from player class 
 *   @param Player x = player object
-* 
+*   @return int with player score
 */
  public int gameLogic(ArrayList<Integer> array, Player x)
  {
@@ -115,27 +52,25 @@ public class CeeLo
       playerScore = array.get(0) * 10;
    
    }
-   // No score: roll, print roll, recursive call to gameLogic with new die values
+   // No score: set score to 0
    else
    {
-      System.out.println(x.getName() + " got: nothing. Re-rolling...");
-      x.playerRolls();
-      showRoll(x);
-      playerScore = gameLogic(x.getDieValues(), x);
+     playerScore = 0;
+     
    }
       x.getDieValues().clear();//empty arraylist so it doesn't get filled dynamically on next roll and same roll returned
  
  return playerScore;
  }
 /**
-*   calculateWinner compares the scores attained by human and player and
-* returns string with winner's name.
+*   calculateWinner compares the scores attained by human and player 
+*   
 *
 *   @param int a = human's score
 *   @param int b = computer's score
 *   @param Player x = human player
 *   @param Player ai = computer
-*   
+*   @return string with winner's name
 */
  public String calculateWinner(int a, int b, Player x, Player ai)
  {
@@ -152,7 +87,7 @@ public class CeeLo
    else
    {
       System.out.println("Tie!");
-      winner = "Nobody";
+      winner = "Nobody: Tie!";
    }
    return winner;
  }
@@ -164,7 +99,7 @@ public class CeeLo
 * @param double wager = bet amount
 * @param Player x = human player
 * @param Player ai = computer
-*
+* @return max 
 */
  
  public double adjustWallet(String s, double max, double wager, Player x, Player ai)
@@ -182,159 +117,28 @@ public class CeeLo
 
 
 /**
-*   showRoll prints out player's roll values.
+*   showRoll get's player's die values
 *   
 *   @param Player x = player object instance
 *   
 */
- public void showRoll(Player x)
+
+public int[] showRoll(Player x)
  {    
       final int SIZE = 3;
-      System.out.println(x.getName() + " rolls... \n");
-         
+      
+      int[] rollArray = new int[SIZE];
+      
       for (int i = 0; i < SIZE; i++)
       {
-         System.out.print(x.getDieValues().get(i) + " ");
+        rollArray[i] = x.getDieValues().get(i);
+         
       } 
       
-      System.out.println("\n");
+     return rollArray;
       
  }
-/**
-*     The input method takes in the amount a player wants to bet as a
-*  <code>String</code> and returns it as a <code>double</code> if it's more than
-*  zero and within the wallet amount.
-*
-* @param double max = wallet amount
-* @return value : amount player wants to bet
-*/
-public double input(double max) throws IOException
-   {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      double value;
-      
-      value = Double.parseDouble(br.readLine());
-      
-      if(value < 0 || value > max)
-        
-         value = validation(value, max);
-        
-      return value;
-   }
-/**
-*  validation tells player if they input an amount 0 or less, or more than they have in the wallet; 
-*  retakes input until conditions satisfied. 
-*
-*
-* @param double x : amount a player wants to bet
-* @param double max : wallet amount
-
-  @return x : amount the player wants to bet - returns as <code>double</code>
-*/   
-public double validation(double x, double max) throws IOException
-   {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-      
-      while( x <= 0 || x > max)
-      {
-         System.out.println("\nValue must be greater than 0! Max bet = whole wallet.\n");
-         x = Double.parseDouble(br.readLine());
-         
-      }
-   
-      return x;
-
-  }
 
 
 }
-
-
-class Die 
-{
-   private int sides;
-   private int value;
-   
-   Die(int numSides)
-   {
-      sides = numSides;
-      roll();
-      
-   }
-   
-   void roll() 
-   {
-      Random rand = new Random();
-      value = rand.nextInt(sides) + 1;
-   }
-   
-   int getSides()
-   {
-      return sides;
-   }
-   
-   int getValue()
-   {
-      return value;
-   
-   }
-
-}
-
-class Player
-{  
-   private String name;
-   private ArrayList<Integer> dieValues;
-   
-   Player()
-   {
-      this.dieValues = new ArrayList<>();
-   
-   }
-
-  
-   
-   private final int numDie = 3;
-   private final int numSides = 6;
-
-   void playerRolls()
-   {
-      ArrayList<Die> dieArray = new ArrayList<>(numDie);
-             
-      
-      for (int i = 0; i < numDie; i++)
-      {
-         dieArray.add(new Die(numSides));
-         
-      }
-      
-      for (int i = 0; i < numDie; i++)
-      {
-         
-         dieValues.add(i, dieArray.get(i).getValue());
-      }
-      
-         Collections.sort(dieValues); 
-         dieArray.clear();
-    } 
-    
-    
-    ArrayList<Integer> getDieValues()
-    {
-      return dieValues;
-    }
-    
-    void setName(String name)
-    {
-      this.name = name;
-    }
-    
-    String getName()
-    {
-      return name;
-    }
-}
-
-
 
